260c859ff328b8d8c3e290a2cd1b0b5c584e404f
RyLai <ryanlai@kkcompany.com>
2023-11-18 12:55:32 +0800

    台北市行政區經緯度轉換

---
+++ Knowledge Management/get_district.py
@@ -0,0 +1,32 @@
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import json
+import os
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+taipei_districts = {}
+dir = "./Knowledge Management/towns/台北市"
+for filename in os.listdir(dir):
+    if filename.endswith(".json"):
+        file_path = os.path.join(dir, filename)
+        with open(file_path) as f:
+            coordinates = json.load(f)
+            taipei_districts[filename[:-5]] = coordinates
+
+taipei_districts["test"] = [(0, 0), (2, 0), (2, 2), (0, 2)]
+
+
+# Function to check if points are within the district
+def is_point_in_district(x, y, district):
+    district_polygon = Polygon(taipei_districts[district])
+    district_gdf = gpd.GeoDataFrame([1], geometry=[district_polygon])
+    print(district_gdf)
+    point = Point(x, y)
+    return district_gdf.contains(point).bool()
+
+
+# # Example usage
+print(is_point_in_district(25.1942, 121.578, "士林區"))
