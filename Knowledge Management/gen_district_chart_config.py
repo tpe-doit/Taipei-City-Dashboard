b30df51dc561df67c93f2edcd409f009a158fc02
RyLai <ryanlai@kkcompany.com>
2023-11-18 18:25:51 +0800

    utility

--- Knowledge Management/gen_district_chart_config.py
+++ Knowledge Management/gen_district_chart_config.py
@@ -1,4 +1,4 @@
-from get_district import is_point_in_district
+from get_district import is_point_in_district, is_point_inside_polygon
 import json
 
 districts = [
@@ -81,37 +81,42 @@ example_data = {
 }
 
 
-geojson_path = "./public/mapData/tp_flood.geojson"
+def calculate_center(points):
+    sum_x = sum(point[0] for point in points)
+    sum_y = sum(point[1] for point in points)
+    num_points = len(points)
+    center_x = sum_x / num_points
+    center_y = sum_y / num_points
+    return center_x, center_y
 
 
-def multiPolygon_config(coordinates):
-    data = { "name": "", "data": [0] * len(coordinates)}
-    for cord in coordinates:
-        area = cord[0]
-        for boundary in area:
-            x = boundary[0]
-            y = boundary[1]
-            for district in districts:
-                if is_point_in_district(x, y, district):
-                    
-                    
+def multiPolygon_data(coordinates):
+    data = [0] * len(districts)
+    for i, district in enumerate(districts):
+        for cord in coordinates:
+            area = cord[0]
+            c_y, c_x = calculate_center(area)
+            if is_point_inside_polygon(c_x, c_y, district):
+                data[i] += 1
+    return data
 
 
 def gen_district_data_from_geojson(file_path):
+    ret_data = {}
     with open(file_path) as f:
         geojson = json.load(f)
         for feature in geojson["features"]:
             # name is the category of the BarPercentChart
             name = feature["properties"]["Name"]
+            ret_data["name"] = name
             if feature["geometry"]["type"] == "MultiPolygon":
-                return multiPolygon_config(feature["geometry"]["coordinates"], name)
-
+                ret_data["data"] = multiPolygon_data(feature["geometry"]["coordinates"])
             else:
                 print("Not Implemented")
-                # try:
-                #     area = sum([float(num) for num in gdf["面積"]])
-                # except:
-                #     area = sum(gdf.geometry.area)
+    return ret_data
 
 
-gen_district_data_from_geojson(geojson_path)
+geojson_path = "./public/mapData/tp_flood.geojson"
+ret = gen_district_data_from_geojson(geojson_path)
+with open("./public/chartData/new_file.json", "w+") as f:
+    json.dump(ret, f)
--- Knowledge Management/get_district.py
+++ Knowledge Management/get_district.py
@@ -28,5 +28,24 @@ def is_point_in_district(x, y, district):
     return district_gdf.contains(point).bool()
 
 
+def is_point_inside_polygon(x, y, district):
+    num = len(taipei_districts[district])
+    inside = False
+
+    p1x, p1y = taipei_districts[district][0]
+    for i in range(num + 1):
+        p2x, p2y = taipei_districts[district][i % num]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
+
 # # Example usage
 # print(is_point_in_district(25.1942, 121.578, "士林區"))
