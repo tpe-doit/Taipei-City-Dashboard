4c6963a113be452cf9edce29fb2d8af78e57ff71
Wen-Xin <u10816006@go.utaipei.edu.tw>
2023-11-19 04:58:07 +0800

    完成funnel chart

--- src/components/charts/FunnelChart.vue
+++ src/components/charts/FunnelChart.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from "vue";
-import { useMapStore } from "../../store/mapStore";
 
 const props = defineProps([
 	"chart_config",
@@ -8,18 +7,17 @@ const props = defineProps([
 	"series",
 	"map_config",
 ]);
-const mapStore = useMapStore();
 
 const chartOptions = ref({
 	chart: {
 		type: "bar",
-		height: 350,
+		height: 300,
+		toolbar: false,
 	},
 	plotOptions: {
 		bar: {
 			borderRadius: 0,
 			horizontal: true,
-			barHeight: "80%",
 			isFunnel: true,
 		},
 	},
@@ -32,56 +30,38 @@ const chartOptions = ref({
 			enabled: true,
 		},
 	},
-	title: {
-		text: "Recruitment Funnel",
-		align: "center",
-	},
 	xaxis: {
-		categories: [
-			"Sourced",
-			"Screened",
-			"Assessed",
-			"HR Interview",
-			"Technical",
-			"Verify",
-			"Offered",
-			"Hired",
-		],
+		categories: props.chart_config.categories,
 	},
 	legend: {
 		show: false,
 	},
+	tooltip: {
+		custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+			const value = props.chart_config.useAbs
+				? Math.abs(series[seriesIndex][dataPointIndex])
+				: series[seriesIndex][dataPointIndex];
+			return (
+				'<div class="chart-tooltip">' +
+				"<h6>" +
+				w.globals.labels[dataPointIndex] +
+				"</h6>" +
+				"<span>" +
+				value +
+				` ${props.chart_config.unit}` +
+				"</span>" +
+				"</div>"
+			);
+		},
+		followCursor: true,
+	},
 });
-
-const chartHeight = 350;
-
-const selectedIndex = ref(null);
-
-function handleDataSelection(e, chartContext, config) {
-	if (!props.chart_config.map_filter) {
-		return;
-	}
-	if (config.dataPointIndex !== selectedIndex.value) {
-		mapStore.addLayerFilter(
-			`${props.map_config[0].index}-${props.map_config[0].type}`,
-			props.chart_config.map_filter[0],
-			props.chart_config.map_filter[1][config.dataPointIndex]
-		);
-		selectedIndex.value = config.dataPointIndex;
-	} else {
-		mapStore.clearLayerFilter(
-			`${props.map_config[0].index}-${props.map_config[0].type}`
-		);
-		selectedIndex.value = null;
-	}
-}
 </script>
 
 <template>
 	<div v-if="activeChart === 'FunnelChart'">
 		<apexchart
 			width="100%"
-			:height="chartHeight"
 			type="bar"
 			:options="chartOptions"
 			:series="series"
