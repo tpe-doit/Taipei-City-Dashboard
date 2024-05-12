fef3674ce11dc571a3c1c4bbf013d2b597d37989
Steve Chang <qqwwee2006@gmail.com>
2024-05-12 10:04:13 +0800

    fix calculate bug

--- Taipei-City-Dashboard-BE/app/controllers/score.go
+++ Taipei-City-Dashboard-BE/app/controllers/score.go
@@ -21,24 +21,30 @@ type GeoJSONFeature struct {
 	} `json:"geometry"`
 }
 
-const earthRadius = 6371 // 地球半徑 km公里
+func Rad2Deg(radians float64) float64 {
+    return radians * 180 / math.Pi
+}
 
-func haversine(lat1, lon1, lat2, lon2 float64) float64 {
-	lat1Rad := lat1 * math.Pi / 180
-	lon1Rad := lon1 * math.Pi / 180
-	lat2Rad := lat2 * math.Pi / 180
-	lon2Rad := lon2 * math.Pi / 180
-	deltaLat := lat2Rad - lat1Rad
-	deltaLon := lon2Rad - lon1Rad
-	a := math.Pow(math.Sin(deltaLat/2), 2) + math.Cos(lat1Rad)*math.Cos(lat2Rad) * math.Pow(math.Sin(deltaLon/2), 2)
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-	distance := earthRadius * c
-	return distance
+func Deg2Rad(degrees float64) float64 {
+    return degrees * math.Pi / 180
+}
+
+func haversine(longitude1, latitude1, longitude2, latitude2 float64) float64 {
+    theta := longitude1 - longitude2
+
+    distance := 60 * 1.1515 * Rad2Deg(
+        math.Acos(
+            (math.Sin(Deg2Rad(latitude1)) * math.Sin(Deg2Rad(latitude2))) +
+            (math.Cos(Deg2Rad(latitude1)) * math.Cos(Deg2Rad(latitude2)) * math.Cos(Deg2Rad(theta))),
+        ),
+    )
+
+	return math.Round(distance*1.609344*100) / 100
 }
 
 // Point 代表一個經緯度的點
 type Point struct {
-	Latitude, Longitude float64
+	Longitude, Latitude float64
 }
 
 // DistanceToPolygon 計算一個點到一個多邊形的最短距離
@@ -53,8 +59,8 @@ func DistanceToPolygon(point Point, polygon []Point) float64 {
 }
 
 /**
-	x: x value for coordinates
-	y: y value for coordinates
+	x: value for longitude
+	y: value for latitude 
 	filename: file name of data
 	weight: weight rate e.g. 0.07
 	calcRange: calculate range
@@ -100,9 +106,9 @@ func GetWeightScore(x float64, y float64, filename string,
 }
 
 func GetLiveSafeScore(c *gin.Context) {
-	// 從請求中獲取 x 和 y 座標
-	xStr := c.Query("x")
-	yStr := c.Query("y")
+	// 從請求中獲取 longitude 和 latitude 座標
+	xStr := c.Query("x") // longitude
+	yStr := c.Query("y") // latitude
 
 	// 將座標轉換為 float64
 	x, err := strconv.ParseFloat(xStr, 64)
@@ -115,7 +121,7 @@ func GetLiveSafeScore(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err})
 		return
 	}
-	result := GetWeightScore(x, y, "traffic_accident_location_view.geojson", 0.00084, 5)
+	result := GetWeightScore(x, y, "traffic_accident_location_view.geojson", 0.00284, 5)
     if (result > 100) {
 		result = 100
 	}
