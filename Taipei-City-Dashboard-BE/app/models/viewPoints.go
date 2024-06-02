459de9b8bf7f356ab4bb8213d5b7216acb8adfb8
hannah_huang <hannah_huang@tengyuntech.com>
2024-06-02 16:58:34 +0800

    feat: migrate view points

--- Taipei-City-Dashboard-BE/app/models/viewPoints.go
+++ Taipei-City-Dashboard-BE/app/models/viewPoints.go
@@ -1,15 +1,16 @@
 package models
 
 type ViewPoints struct {
-	ID        int     `json:"id" gorm:"column:id;autoincrement;primaryKey"`
-	UserID    int     `json:"user_id" gorm:"column:user_id;not null"`
-	CenterX   float32 `json:"center_x" gorm:"column:center_x"`
-	CenterY   float32 `json:"center_y" gorm:"column:center_y"`
-	Zoom      float32 `json:"zoom" gorm:"column:zoom"`
-	Pitch     float32 `json:"pitch" gorm:"column:pitch"`
-	Bearing   float32 `json:"bearing" gorm:"column:bearing"`
-	Name      string  `json:"name" gorm:"column:name"`
-	PointType string  `json:"point_type" gorm:"column:point_type"`
+	ID        int      `json:"id" gorm:"column:id;autoincrement;primaryKey"`
+	UserID    int      `json:"user_id" gorm:"column:user_id;not null"`
+	CenterX   float32  `json:"center_x" gorm:"column:center_x"`
+	CenterY   float32  `json:"center_y" gorm:"column:center_y"`
+	Zoom      float32  `json:"zoom" gorm:"column:zoom"`
+	Pitch     float32  `json:"pitch" gorm:"column:pitch"`
+	Bearing   float32  `json:"bearing" gorm:"column:bearing"`
+	Name      string   `json:"name" gorm:"column:name"`
+	PointType string   `json:"point_type" gorm:"column:point_type"`
+	AuthUser  AuthUser `gorm:"foreignKey:UserID"`
 }
 
 func CreateViewPoint(user_id int, center_x float32, center_y float32, zoom float32, pitch float32, bearing float32, name string, pointType string) (ViewPoints, error) {
