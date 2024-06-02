ba0f9e3a3b1c023b64cdbe43554ae4b7de82e116
rakechen-0307 <rakechen168@gmail.com>
2024-06-02 13:28:11 +0800

    frontend linting

--- Taipei-City-Dashboard-FE/src/components/map/MapContainer.vue
+++ Taipei-City-Dashboard-FE/src/components/map/MapContainer.vue
@@ -50,110 +50,113 @@ const showTooltip = ref(false);
 </script>
 
 <template>
-	<div class="mapcontainer">
-		<div class="mapcontainer-map">
-			<!-- #mapboxBox needs to be empty to ensure Mapbox performance -->
-			<div id="mapboxBox" />
-			<div
-				v-if="mapStore.loadingLayers.length > 0"
-				class="mapcontainer-loading"
-			>
-				<div />
-			</div>
-			<div class="mapcontainer-layers">
-				<button
-					:style="{
-						color: districtLayer
-							? 'var(--color-highlight)'
-							: 'var(--color-component-background)',
-					}"
-					@click="toggleDistrictLayer"
-				>
-					區
-				</button>
-				<button
-					:style="{
-						color: villageLayer
-							? 'var(--color-highlight)'
-							: 'var(--color-component-background)',
-					}"
-					@click="toggleVillageLayer"
-				>
-					里
-				</button>
+  <div class="mapcontainer">
+    <div class="mapcontainer-map">
+      <!-- #mapboxBox needs to be empty to ensure Mapbox performance -->
+      <div id="mapboxBox" />
+      <div
+        v-if="mapStore.loadingLayers.length > 0"
+        class="mapcontainer-loading"
+      >
+        <div />
+      </div>
+      <div class="mapcontainer-layers">
+        <button
+          :style="{
+            color: districtLayer
+              ? 'var(--color-highlight)'
+              : 'var(--color-component-background)',
+          }"
+          @click="toggleDistrictLayer"
+        >
+          區
+        </button>
+        <button
+          :style="{
+            color: villageLayer
+              ? 'var(--color-highlight)'
+              : 'var(--color-component-background)',
+          }"
+          @click="toggleVillageLayer"
+        >
+          里
+        </button>
 
-				<button
-					v-if="
-						mapConfigsLength === 1 &&
-						mapConfigs[currentVisibleLayerKey ?? '']?.type ===
-							'circle'
-					"
-					:style="{
-						color: villageLayer
-							? 'var(--color-highlight)'
-							: 'var(--color-component-background)',
-					}"
-					type="button"
-					@click="mapStore.flyToClosestLocationAndTriggerPopup"
-				>
-					近
-				</button>
-				<button
-					class="show-if-mobile"
-					@click="dialogStore.showDialog('mobileLayers')"
-				>
-					<span>layers</span>
-				</button>
-			</div>
-			<!-- The key prop informs vue that the component should be updated when switching dashboards -->
-			<MobileLayers :key="contentStore.currentDashboard.index" />
-			<button
-				class="input"
-				:style="{
-					// color: villageLayer
-					// 	? 'var(--color-highlight)'
-					// 	: 'var(--color-component-background)'
-				}"
-				title="通報災害"
-				@click="dialogStore.showDialog('incidentReport')"
-				@mouseover="showTooltip = true"
-				@mouseleave="showTooltip = false"
-			>
-				<!-- <span class="material-symbols-outlined icon">e911_emergency</span> -->
-				<span v-if="showTooltip" class="tooltip">通報災害</span>
-				!
-			</button>
-			<IncidentReport />
-		</div>
+        <button
+          v-if="
+            mapConfigsLength === 1 &&
+              mapConfigs[currentVisibleLayerKey ?? '']?.type ===
+              'circle'
+          "
+          :style="{
+            color: villageLayer
+              ? 'var(--color-highlight)'
+              : 'var(--color-component-background)',
+          }"
+          type="button"
+          @click="mapStore.flyToClosestLocationAndTriggerPopup"
+        >
+          近
+        </button>
+        <button
+          class="show-if-mobile"
+          @click="dialogStore.showDialog('mobileLayers')"
+        >
+          <span>layers</span>
+        </button>
+      </div>
+      <!-- The key prop informs vue that the component should be updated when switching dashboards -->
+      <MobileLayers :key="contentStore.currentDashboard.index" />
+      <button
+        class="input"
+        :style="{
+          // color: villageLayer
+          // 	? 'var(--color-highlight)'
+          // 	: 'var(--color-component-background)'
+        }"
+        title="通報災害"
+        @click="dialogStore.showDialog('incidentReport')"
+        @mouseover="showTooltip = true"
+        @mouseleave="showTooltip = false"
+      >
+        <!-- <span class="material-symbols-outlined icon">e911_emergency</span> -->
+        <span
+          v-if="showTooltip"
+          class="tooltip"
+        >通報災害</span>
+        !
+      </button>
+      <IncidentReport />
+    </div>
 
-		<div class="mapcontainer-controls hide-if-mobile">
-			<button
-				@click="
-					mapStore.easeToLocation([
-						[121.536609, 25.044808],
-						12.5,
-						0,
-						0,
-					])
-				"
-			>
-				返回預設
-			</button>
-			<div
-				v-for="(item, index) in mapStore.savedLocations"
-				:key="`${item[4]}-${index}`"
-			>
-				<button @click="mapStore.easeToLocation(item)">
-					{{ item[4] }}
-				</button>
-				<!-- <div
+    <div class="mapcontainer-controls hide-if-mobile">
+      <button
+        @click="
+          mapStore.easeToLocation([
+            [121.536609, 25.044808],
+            12.5,
+            0,
+            0,
+          ])
+        "
+      >
+        返回預設
+      </button>
+      <div
+        v-for="(item, index) in mapStore.savedLocations"
+        :key="`${item[4]}-${index}`"
+      >
+        <button @click="mapStore.easeToLocation(item)">
+          {{ item[4] }}
+        </button>
+        <!-- <div
 					class="mapcontainer-controls-delete"
 					@click="mapStore.removeSavedLocation(index)"
 				>
 					<span>delete</span>
 				</div> -->
-			</div>
-			<!-- <input
+      </div>
+      <!-- <input
 				v-if="mapStore.savedLocations.length < 10"
 				type="text"
 				placeholder="新增後按Enter"
@@ -162,8 +165,8 @@ const showTooltip = ref(false);
 				@focusout="newSavedLocation = ''"
 				@keypress.enter="handleSubmitNewLocation"
 			/> -->
-		</div>
-	</div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
