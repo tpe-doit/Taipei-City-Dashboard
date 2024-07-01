5ec3d0ffab1491080e36c7c518a02e2b3f15d300
Iima <earl790314@gmail.com>
2024-07-01 10:41:49 +0800

    Add R0064

---
+++ Taipei-City-Dashboard-DE/dags/proj_city_dashboard/R0064/R0064.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from operators.common_pipeline import CommonDag
+
+
+def _R0064(**kwargs):
+    from airflow.models import Variable
+    from proj_city_dashboard.R0062.renew_etl import renew_etl
+
+    # Config
+    URL = Variable.get("R0064_URL", "")
+    GEOMETRY_TYPE = "MultiPolygon"
+    FROM_CRS = 4326
+
+    # ETL
+    renew_etl(URL, FROM_CRS, GEOMETRY_TYPE, **kwargs)
+
+
+dag = CommonDag(proj_folder="proj_city_dashboard", dag_folder="R0064")
+dag.create_dag(etl_func=_R0064)
