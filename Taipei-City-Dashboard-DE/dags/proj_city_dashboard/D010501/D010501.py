1fbae0b5b21497ca7a66bb1b659b2160de829e30
Iima <earl790314@gmail.com>
2024-07-01 10:37:00 +0800

    Add D010501

---
+++ Taipei-City-Dashboard-DE/dags/proj_city_dashboard/D010501/D010501.py
@@ -0,0 +1,119 @@
+from airflow import DAG
+from operators.common_pipeline import CommonDag
+
+
+def _D010501(**kwargs):
+    import pandas as pd
+    import requests
+    from sqlalchemy import create_engine
+    from utils.extract_stage import get_data_taipei_api
+    from utils.load_stage import (
+        save_dataframe_to_postgresql,
+        update_lasttime_in_data_to_dataset_info,
+    )
+    from utils.transform_time import convert_str_to_time_format
+
+    def get_datataipei_file_rid(page_id):
+        url = f"https://data.taipei/api/frontstage/tpeod/dataset.view?id={page_id}"
+        res = requests.get(url)
+        res.raise_for_status()
+        res_json = res.json()
+        data_list = res_json["payload"]["resources"]
+        url_list = {}
+        for data in data_list:
+            file_tag = data["name"][0:5]
+            rid = data["rid"]
+            url_list[file_tag] = rid
+        return url_list
+
+    def get_existing_data(ready_data_db_uri, table_name, column="file_tag"):
+        engine = create_engine(ready_data_db_uri)
+        sql = f"select distinct {column} from {table_name}"
+        existing_tag = pd.read_sql(sql, engine).iloc[:, 0]
+        return existing_tag.tolist()
+
+    # Config
+    ready_data_db_uri = kwargs.get("ready_data_db_uri")
+    dag_infos = kwargs.get("dag_infos")
+    dag_id = dag_infos.get("dag_id")
+    load_behavior = dag_infos.get("load_behavior")
+    default_table = dag_infos.get("ready_data_default_table")
+    history_table = dag_infos.get("ready_data_history_table")
+    PAGE_ID = "cb423c17-88eb-4231-a725-f1b93247d1bf"
+
+    # Extract
+    # get all data url
+    data_list = get_datataipei_file_rid(PAGE_ID)
+    # filter out existing year
+    existing_month = get_existing_data(ready_data_db_uri, default_table)
+    new_data_list = {k: v for k, v in data_list.items() if k not in existing_month}
+    # get data
+    raw_datas = []
+    for file_tag, rid in new_data_list.items():
+        raw_data = get_data_taipei_api(rid, output_format="dataframe")
+        raw_data["file_tag"] = file_tag
+        raw_datas.append(raw_data)
+
+    # Transform
+    ready_datas = []
+    for raw_data in raw_datas:
+        data = raw_data.copy()
+        # rename
+        data = data.rename(
+            columns={
+                "file_tag": "file_tag",
+                "案件編號": "case_no",
+                "案件主類別": "main_category",
+                "案件次類別": "sub_category",
+                "案件子類別": "sub_category",
+                "受理機關": "assign_agency",
+                "受理科室": "assign_dept",
+                "受理日期": "accept_time",
+                "送達日期": "apply_time",
+                "結案日期": "close_time",
+                "data_time": "data_time",
+            }
+        )
+        # fill missing columns, cause old data may not have all columns
+        select_columns = [
+            "data_time",
+            "file_tag",
+            "case_no",
+            "main_category",
+            "sub_category",
+            "assign_agency",
+            "assign_dept",
+            "accept_time",
+            "apply_time",
+            "close_time",
+        ]
+        for col in select_columns:
+            if col not in list(data.columns):
+                data[col] = None
+        # standardize time
+        data["data_time"] = convert_str_to_time_format(data["data_time"])
+        data["accept_time"] = convert_str_to_time_format(
+            data["accept_time"], errors="coerce"
+        )
+        data["apply_time"] = convert_str_to_time_format(data["apply_time"])
+        data["close_time"] = convert_str_to_time_format(data["close_time"])
+        # select columns
+        ready_data = data[select_columns]
+        ready_datas.append(ready_data)
+
+    # Load
+    engine = create_engine(ready_data_db_uri)
+    for ready_data in ready_datas:
+        save_dataframe_to_postgresql(
+            engine,
+            data=ready_data,
+            load_behavior=load_behavior,
+            default_table=default_table,
+        )
+        update_lasttime_in_data_to_dataset_info(
+            engine, dag_id, data["close_time"].max()
+        )
+
+
+dag = CommonDag(proj_folder="proj_city_dashboard", dag_folder="D010501")
+dag.create_dag(etl_func=_D010501)
